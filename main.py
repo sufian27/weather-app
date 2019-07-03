664ff0700637e2b2edcbe6351356dace421e65b3
Sufian Mushtaq <sufian27@users.noreply.github.com>
2019-07-03 15:13:14 +0500

    added key security

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import requests
 import json
+from genkeys import generate_key
 
 def main():
 
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
-    key = "71dc8342d714cc6d6a2e54b2eb8765a8" #add your key here
+    weather_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    ip_url = "http://api.ipstack.com/check?access_key={}"
+    
+    ###### ADD YOUR KEYS HERE, THE PROGRAM WON'T WORK WITHOUT IT #####
+    weather_key = generate_key("weather")
+    ip_key = generate_key("ip") 
+    ##################################################################
+
     city = input("Enter city: ")
-    r = requests.get(url.format(city, key))
-    jsonfile = json.loads(r.text)
+    r_weather = requests.get(weather_url.format(city, weather_key))
+    json_file = json.loads(r_weather.text)
     while jsonfile["cod"] == "404": 
         print("City not found")
         city = input("Enter city: ")
-        r = requests.get(url.format(city, key))
-        jsonfile = json.loads(r.text)
+        r = requests.get(weather_url.format(city, key))
+        json_file = json.loads(r.text)
     print('''
     Location: {}
     Weather: {}
     Current Temperature: {} C 
     Min Temperature: {} C
     Max Temperature {} C
-    '''.format(city, jsonfile["weather"][0]["main"], jsonfile["main"]["temp"], jsonfile["main"]["temp_min"], 
-    jsonfile["main"]["temp_max"]))
+    '''.format(city, json_file["weather"][0]["main"], json_file["main"]["temp"], json_file["main"]["temp_min"], 
+    json_file["main"]["temp_max"]))
 
 if __name__ == "__main__":
     main()
