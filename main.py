2070991d9fe635c5fcedf8795751a8c7d6f45948
Sufian Mushtaq <sufian27@users.noreply.github.com>
2019-07-03 14:30:22 +0500

    parsed json file to print temperature of city entered

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def main():
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=71dc8342d714cc6d6a2e54b2eb8765a8"
     city = input("Enter city: ")
     r = requests.get(url.format(city))
-    print(r.text)
+    jsonfile = json.loads(r.text)
+    print('''
+    Location: {}
+    Weather: {}
+    Current Temperature: {} C 
+    '''.format(city, jsonfile["weather"][0]["main"], jsonfile["main"]["temp"]))
 
 if __name__ == "__main__":
     main()
