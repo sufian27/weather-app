ccc5152b7f2e5d22e61845bd866df25a8520e91a
Sufian Mushtaq <sufian27@users.noreply.github.com>
2019-07-03 14:49:28 +0500

    added error checks and min and max temperature in output

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ def main():
     city = input("Enter city: ")
     r = requests.get(url.format(city))
     jsonfile = json.loads(r.text)
+    while jsonfile["cod"] == "404": 
+        print("City not found")
+        city = input("Enter city: ")
+        r = requests.get(url.format(city))
+        jsonfile = json.loads(r.text)
     print('''
     Location: {}
     Weather: {}
     Current Temperature: {} C 
-    '''.format(city, jsonfile["weather"][0]["main"], jsonfile["main"]["temp"]))
+    Min Temperature: {} C
+    Max Temperature {} C
+    '''.format(city, jsonfile["weather"][0]["main"], jsonfile["main"]["temp"], jsonfile["main"]["temp_min"], 
+    jsonfile["main"]["temp_max"]))
 
 if __name__ == "__main__":
     main()
