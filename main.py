cd71965e97858b84644e4b275ecead2e596a9e9f
Sufian Mushtaq <sufian27@users.noreply.github.com>
2019-07-03 15:39:02 +0500

    added real-time weather output using the user's ip's location

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from genkeys import generate_key
 
 def main():
 
-    weather_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+    weather_url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=metric&APPID={}"
     ip_url = "http://api.ipstack.com/check?access_key={}"
     
     ###### ADD YOUR KEYS HERE, THE PROGRAM WON'T WORK WITHOUT IT #####
@@ -12,22 +12,23 @@ def main():
     ip_key = generate_key("ip") 
     ##################################################################
 
-    city = input("Enter city: ")
-    r_weather = requests.get(weather_url.format(city, weather_key))
-    json_file = json.loads(r_weather.text)
-    while jsonfile["cod"] == "404": 
-        print("City not found")
-        city = input("Enter city: ")
-        r = requests.get(weather_url.format(city, key))
-        json_file = json.loads(r.text)
-    print('''
-    Location: {}
-    Weather: {}
-    Current Temperature: {} C 
-    Min Temperature: {} C
-    Max Temperature {} C
-    '''.format(city, json_file["weather"][0]["main"], json_file["main"]["temp"], json_file["main"]["temp_min"], 
-    json_file["main"]["temp_max"]))
+    r_ip = requests.get(ip_url.format(ip_key))
+    json_file_ip = json.loads(r_ip.text)
+    user_lat = json_file_ip["latitude"]
+    user_long = json_file_ip["longitude"]
+    r_weather = requests.get(weather_url.format(user_lat, user_long, weather_key))
+    json_file_weather = json.loads(r_weather.text)
+    if json_file_weather["cod"] == "404": 
+        print("Location not found")
+    else: 
+        print('''
+        Your Location: {}
+        Weather: {}
+        Current Temperature: {} C 
+        Min Temperature: {} C
+        Max Temperature {} C
+        '''.format(json_file_weather["name"], json_file_weather["weather"][0]["main"], json_file_weather["main"]["temp"], 
+        json_file_weather["main"]["temp_min"], json_file_weather["main"]["temp_max"]))
 
 if __name__ == "__main__":
     main()
