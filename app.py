caf5c0c11cf21a9e34fc209d7072de91043f1eca
Sufian Mushtaq <sufian27@users.noreply.github.com>
2019-07-09 15:25:07 +0500

    Added client side script to get location of client

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from weather import get_weather
 app = Flask(__name__)
 
-@app.route("/")
-def run():
+@app.route("/", methods=["GET", "POST"])
+def index():
     json_file_weather = get_weather()
+    if request.method == "POST": 
+        print(request.form["latitude"])
+        print(request.form["longitude"])
     return render_template("index.html", location=json_file_weather["name"], 
-    weather=json_file_weather["weather"][0]["main"], 
+    weather=json_file_weather["weather"][0]["description"], 
     temp_curr=json_file_weather["main"]["temp"], temp_min=json_file_weather["main"]["temp_min"], 
     temp_max=json_file_weather["main"]["temp_max"])
 
